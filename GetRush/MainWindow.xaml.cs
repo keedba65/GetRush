e73e5a8bbecaed9689c5dee4849e3956525a05a2
Kevin Barlow <kbarlow@egnyte.com>
2017-06-20 12:56:39 -0700

    Converted to WPF desktop application.

--- GetRush/MainWindow.xaml.cs
+++ GetRush/MainWindow.xaml.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using System.Xml.Serialization;
 
 namespace GetRush
 {
-    class Program
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        static async void Doit()
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetFeedButton_Click(object sender, RoutedEventArgs e)
         {
             RushPodcast podcast = new RushPodcast();
             string result = await podcast.GetPodcast();
@@ -24,22 +40,19 @@ namespace GetRush
             }
             DateTime dtLast = podcast.GetLastDownloadTimestamp();
             Stack<RssItem> feedStack = new Stack<RssItem>();
-            foreach(var item in feed.Channel.Item)
+            foreach (var item in feed.Channel.Item)
             {
                 if (item.PubDate <= dtLast) break;
                 feedStack.Push(item);
             }
-            while(feedStack.Count > 0)
+            StringBuilder sb = new StringBuilder();
+            while (feedStack.Count > 0)
             {
                 var item = feedStack.Pop();
+                sb.AppendLine($"Got {item.Title}");
                 await podcast.DownloadItem(item);
             }
-            Console.WriteLine(result);
-        }
-        static void Main(string[] args)
-        {
-            Doit();
-            Console.ReadLine();
+            MessageBox.Show(sb.ToString(), "Download complete");
         }
     }
 }
